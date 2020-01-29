2bdffac67de47a8c987a68d7f3d4504e59b57ab1
adrideandrade <adriandeandrade98@gmail.com>
2020-01-28 22:30:07 -0500

    working with pixel perfect ui

--- Assets/_Project/Scripts/MainMenu/Level Select Screen/LevelData.cs
+++ Assets/_Project/Scripts/MainMenu/Level Select Screen/LevelData.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [CreateAssetMenu(fileName = "New Level", menuName = "Menu Options/New Level")]
 public class LevelData : ScriptableObject
---
+++ Assets/_Project/Scripts/MainMenu/MenuButton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Button))]
+public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Sprite hoveredSprite;
+    private Button button;
+    private TextMeshProUGUI buttonText;
+
+    private Sprite originalImage;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+        buttonText = GetComponentInChildren<TextMeshProUGUI>();
+        originalImage = button.image.sprite;
+
+        ConfigureButton();
+    }
+
+    private void ConfigureButton()
+    {
+        button.transition = Selectable.Transition.SpriteSwap;
+    }
+
+    public void SetButtonText(string newText)
+    {
+        buttonText.SetText(newText);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        
+    }
+
+    private void SetButtonImage()
+    {
+
+    }
+}
